66a55cc85156f70c9e1e5cdf96e6fbcd5224e58f
Catalin Casuneanu <catalin.casuneanu@jay-software.com>
2024-03-02 14:37:59 +0200

    fix: add subscribers route

--- src/app/api/subscribers/route.ts
+++ src/app/api/subscribers/route.ts
@@ -3,16 +3,16 @@ import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
-  const { name, email, phoneNumber }: Partial<Subscriber> = await req.json();
+  const { name, email, phonenumber }: Partial<Subscriber> = await req.json();
 
-  if (!name || !email || !phoneNumber) {
+  if (!name || !email || !phonenumber) {
     return NextResponse.json({ error: 'Invalid input' }, { status: 400 });
   }
 
   const createdOn = new Date();
   const {rows} = await sql`
     INSERT INTO subscribers (name, email, phoneNumber, isSubscriber, createdOn)
-    VALUES (${name}, ${email}, ${phoneNumber}, true, ${createdOn.toISOString()})
+    VALUES (${name}, ${email}, ${phonenumber}, true, ${createdOn.toISOString()})
   `;
   return NextResponse.json(rows);
 }
